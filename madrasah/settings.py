29c2bc1b940efa277ba665ac5f4c3dcc0e5697ac
Mamurov <74642342+Mamurov@users.noreply.github.com>
2021-10-20 22:45:51 +0600

    Update settings.py

--- madrasah/settings.py
+++ madrasah/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ns_db',
         'USER': 'postgres',
-        'PASSWORD': '1',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432,
     }
